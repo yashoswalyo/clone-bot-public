5576a9d50fdcf9d460e9c1489ec5d186eb24a851
Yash Oswal <yashoswal18@gmail.com>
2022-05-23 20:15:06 +0530

    khudko credit dena toh bhul hi daya

--- bot/__main__.py
+++ bot/__main__.py
@@ -109,9 +109,9 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     buttons.buildbutton(
-        "Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot"
+        "Repo", "https://www.github.com/yashoswalyo/clone-bot-public"
     )
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Report Group", "https://t.me/itzTgDiscussGroup")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f"""
