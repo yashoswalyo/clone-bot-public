620954c14fe6adc2af8c52f23be81c61c509797f
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-05-25 17:21:57 +0530

    Update clone.py

--- bot/functions/clone.py
+++ bot/functions/clone.py
@@ -173,6 +173,9 @@ def cloneNode(update, context):
 
 
 clone_handler = CommandHandler(
-    "clone", cloneNode, filters=CustomFilters.authorized_chat, run_async=True
+    BotCommands.CloneCommand,
+    cloneNode,
+    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
+    run_async=True,
 )
 dispatcher.add_handler(clone_handler)
