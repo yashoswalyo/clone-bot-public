dc054a4c2592360e0bf2eab1db8aa419e78d30e7
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-05-25 15:45:04 +0530

    Update link_generator.py

--- bot/helper/mirror/download/link_generator.py
+++ bot/helper/mirror/download/link_generator.py
@@ -1,35 +1,185 @@
-from bot import CRYPT
+from bot import UNIFIED_EMAIL, UNIFIED_PASS, GDTOT_CRYPT, HUBDRIVE_CRYPT, KATDRIVE_CRYPT, DRIVEFIRE_CRYPT
 from bot.helper.others.exceptions import DirectDownloadLinkException
-from re import (
-    findall as re_findall,
-    sub as re_sub,
-    match as re_match,
-    search as re_search,
-)
-from base64 import b64decode
-from requests import get as rget, head as rhead, post as rpost, Session as rsession
+import re
+import base64
+
+from lxml import etree
+from urllib.parse import urlparse, parse_qs
+import requests
 
 
 def gdtot(url: str) -> str:
-    """Gdtot google drive link generator
-    By https://github.com/xcscxr"""
+    if GDTOT_CRYPT is None:
+        raise DDLException("GDTOT_CRYPT env var not provided")
+    client = requests.Session()
+    client.cookies.update({'crypt': GDTOT_CRYPT})
+    res = client.get(url)
+    res = client.get(f"https://new.gdtot.nl/dld?id={url.split('/')[-1]}")
+    url = re.findall(r'URL=(.*?)"', res.text)[0]
+    info = {}
+    info['error'] = False
+    params = parse_qs(urlparse(url).query)
+    if 'gd' not in params or not params['gd'] or params['gd'][0] == 'false':
+        info['error'] = True
+        if 'msgx' in params:
+            info['message'] = params['msgx'][0]
+        else:
+            info['message'] = 'Invalid link'
+    else:
+        decoded_id = base64.b64decode(str(params['gd'][0])).decode('utf-8')
+        drive_link = f'https://drive.google.com/open?id={decoded_id}'
+        info['gdrive_link'] = drive_link
+    if not info['error']:
+        return info['gdrive_link']
+    else:
+        raise DDLException(f"{info['message']}")
 
-    if CRYPT is None:
-        raise DirectDownloadLinkException("ERROR: CRYPT cookie not provided")
+account = {
+    'email': UNIFIED_EMAIL, 
+    'passwd': UNIFIED_PASS
+}
 
-    match = re_findall(r"https?://(.+)\.gdtot\.(.+)\/\S+\/\S+", url)[0]
+def account_login(client, url, email, password):
+    data = {
+        'email': email,
+        'password': password
+    }
+    client.post(f'https://{urlparse(url).netloc}/login', data=data)
+    
+def gen_payload(data, boundary=f'{"-"*6}_'):
+    data_string = ''
+    for item in data:
+        data_string += f'{boundary}\r\n'
+        data_string += f'Content-Disposition: form-data; name="{item}"\r\n\r\n{data[item]}\r\n'
+    data_string += f'{boundary}--\r\n'
+    return data_string
 
-    with rsession() as client:
-        client.cookies.update({"crypt": CRYPT})
-        res = client.get(url)
-        res = client.get(
-            f"https://{match[0]}.gdtot.{match[1]}/dld?id={url.split('/')[-1]}"
-        )
-    matches = re_findall("gd=(.*?)&", res.text)
+def parse_infou(data):
+    info = re.findall('>(.*?)<\/li>', data)
+    info_parsed = {}
+    for item in info:
+        kv = [s.strip() for s in item.split(':', maxsplit = 1)]
+        info_parsed[kv[0].lower()] = kv[1]
+    return info_parsed
+
+def unified(url: str) -> str:
+    if (UNIFIED_EMAIL or UNIFIED_PASS) is None:
+        raise DDLException("UNIFIED_EMAIL and UNIFIED_PASS env vars not provided")
+    client = requests.Session()
+    client.headers.update({
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+    })
+
+    account_login(client, url, account['email'], account['passwd'])
+
+    res = client.get(url)
+    key = re.findall('"key",\s+"(.*?)"', res.text)[0]
+
+    ddl_btn = etree.HTML(res.content).xpath("//button[@id='drc']")
+
+    info_parsed = parse_infou(res.text)
+    info_parsed['error'] = False
+    info_parsed['link_type'] = 'login' # direct/login
+    
+    headers = {
+        "Content-Type": f"multipart/form-data; boundary={'-'*4}_",
+    }
+    
+    data = {
+        'type': 1,
+        'key': key,
+        'action': 'original'
+    }
+    
+    if len(ddl_btn):
+        info_parsed['link_type'] = 'direct'
+        data['action'] = 'direct'
+    
+    while data['type'] <= 3:
+        try:
+            response = client.post(url, data=gen_payload(data), headers=headers).json()
+            break
+        except: data['type'] += 1
+        
+    if 'url' in response:
+        info_parsed['gdrive_link'] = response['url']
+    elif 'error' in response and response['error']:
+        info_parsed['error'] = True
+        info_parsed['error_message'] = response['message']
+    else:
+        info_parsed['error'] = True
+        info_parsed['error_message'] = 'Something went wrong :('
+    
+    if urlparse(url).netloc == 'appdrive.in' and not info_parsed['error']:
+        return info_parsed['gdrive_link']
+    
+    if urlparse(url).netloc == 'driveapp.in' and not info_parsed['error']:
+        res = client.get(info_parsed['gdrive_link'])
+        drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn')]/@href")[0]
+        info_parsed['gdrive_link'] = drive_link
+
+    if urlparse(url).netloc == 'drivesharer.in' and not info_parsed['error']:
+        res = client.get(info_parsed['gdrive_link'])
+        drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
+        info_parsed['gdrive_link'] = drive_link
+
+    if urlparse(url).netloc == 'drivebit.in' and not info_parsed['error']:
+        res = client.get(info_parsed['gdrive_link'])
+        drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
+        info_parsed['gdrive_link'] = drive_link
+        
+    if urlparse(url).netloc == 'driveace.in' and not info_parsed['error']:
+        res = client.get(info_parsed['gdrive_link'])
+        drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
+        info_parsed['gdrive_link'] = drive_link
+
+
+    info_parsed['src_url'] = url
+    
+    return info_parsed['gdrive_link']
+
+def parse_info(res):
+    info_parsed = {}
+    title = re.findall('>(.*?)<\/h4>', res.text)[0]
+    info_chunks = re.findall('>(.*?)<\/td>', res.text)
+    info_parsed['title'] = title
+    for i in range(0, len(info_chunks), 2):
+        info_parsed[info_chunks[i]] = info_chunks[i+1]
+    return info_parsed
+
+def udrive(url: str) -> str:
+    client = requests.Session()
+    if ('hubdrive' or 'drivehub') in url:
+        client.cookies.update({'crypt': HUBDRIVE_CRYPT})
+    if ('katdrive' or 'kolop') in url:
+        client.cookies.update({'crypt': KATDRIVE_CRYPT})
+    if 'drivefire' in url:
+        client.cookies.update({'crypt': DRIVEFIRE_CRYPT})
+    res = client.get(url)
+    info_parsed = parse_info(res)
+    info_parsed['error'] = False
+    
+    up = urlparse(url)
+    req_url = f"{up.scheme}://{up.netloc}/ajax.php?ajax=download"
+    
+    file_id = url.split('/')[-1]
+    
+    data = { 'id': file_id }
+    
+    headers = {
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    
     try:
-        decoded_id = b64decode(str(matches[0])).decode("utf-8")
-    except:
-        raise DirectDownloadLinkException(
-            "ERROR: Try in your broswer, mostly file not found or user limit exceeded!"
-        )
-    return f"https://drive.google.com/open?id={decoded_id}"
+        res = client.post(req_url, headers=headers, data=data).json()['file']
+    except: return {'error': True, 'src_url': url}
+    
+    if 'drivefire.co' in url:
+      return res
+    else:
+      gd_id = re.findall('gd=(.*)', res, re.DOTALL)[0]
+    
+    info_parsed['gdrive_url'] = f"https://drive.google.com/open?id={gd_id}"
+    info_parsed['src_url'] = url
+
+    return info_parsed['gdrive_url']
