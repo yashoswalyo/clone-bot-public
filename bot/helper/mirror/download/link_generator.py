1d6e41e8eeb3aae5cfd627c7b77ab528259d1a77
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-05-25 17:53:43 +0530

    Update link_generator.py

--- bot/helper/mirror/download/link_generator.py
+++ bot/helper/mirror/download/link_generator.py
@@ -111,32 +111,37 @@ def unified(url: str) -> str:
         info_parsed['error_message'] = 'Something went wrong :('
     
     if urlparse(url).netloc == 'appdrive.in' and not info_parsed['error']:
-        return info_parsed['gdrive_link']
+        flink = info_parsed['gdrive_link']
+        return flink
     
     if urlparse(url).netloc == 'driveapp.in' and not info_parsed['error']:
         res = client.get(info_parsed['gdrive_link'])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn')]/@href")[0]
-        info_parsed['gdrive_link'] = drive_link
+        flink = drive_link
+        return flink
 
     if urlparse(url).netloc == 'drivesharer.in' and not info_parsed['error']:
         res = client.get(info_parsed['gdrive_link'])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
-        info_parsed['gdrive_link'] = drive_link
+        flink = drive_link
+        return flink
 
     if urlparse(url).netloc == 'drivebit.in' and not info_parsed['error']:
         res = client.get(info_parsed['gdrive_link'])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
-        info_parsed['gdrive_link'] = drive_link
+        flink = drive_link
+        return flink
         
     if urlparse(url).netloc == 'driveace.in' and not info_parsed['error']:
         res = client.get(info_parsed['gdrive_link'])
         drive_link = etree.HTML(res.content).xpath("//a[contains(@class,'btn btn-primary')]/@href")[0]
-        info_parsed['gdrive_link'] = drive_link
+        flink = drive_link
+        return flink
 
-
+    flink = info_parsed['gdrive_link']
     info_parsed['src_url'] = url
     
-    return info_parsed['gdrive_link']
+    return flink
 
 def parse_info(res):
     info_parsed = {}
