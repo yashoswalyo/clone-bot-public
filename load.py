87ec898913eb374524dcc204088c6f4075b9674e
Yash Oswal <yashoswal18@gmail.com>
2022-06-01 19:57:29 +0530

    UPSTREAM_REPO, thx @anasty17 for mltb

--- load.py
+++ load.py
@@ -1,6 +1,7 @@
-from os import environ
+from os import environ, path as ospath
+from subprocess import run as srun
 from dotenv import load_dotenv
-from logging import basicConfig, FileHandler, StreamHandler, INFO, error as log_error
+from logging import basicConfig, FileHandler, StreamHandler, INFO, error as log_error, info as log_info
 from requests import get as rget
 
 basicConfig(
@@ -8,7 +9,6 @@ basicConfig(
     handlers=[FileHandler("log.txt"), StreamHandler()],
     level=INFO,
 )
-
 CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
     if len(CONFIG_FILE_URL) == 0:
@@ -28,3 +28,34 @@ load_dotenv(
     "config.env",
     override=True,
 )
+
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
+try:
+    if len(UPSTREAM_REPO) == 0:
+       raise TypeError
+except:
+    UPSTREAM_REPO = None
+try:
+    if len(UPSTREAM_BRANCH) == 0:
+       raise TypeError
+except:
+    UPSTREAM_BRANCH = 'master'
+
+if UPSTREAM_REPO is not None:
+    if ospath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
+
+    update = srun([f"git init -q \
+                     && git config --global user.email yashoswal18@gmail.com \
+                     && git config --global user.name clonebot \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+    if update.returncode == 0:
+        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    else:
+        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
